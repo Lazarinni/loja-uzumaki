d67a2e129ce9b7b455094bf9dbee52643822a0d6
MatheusHenriqueDevz <matheus.silva611@fatec.sp.gov.br>
2024-09-09 11:28:59 -0300

    Atualização ta tela de login e adicionado o sistema de usuarios com json

--- homolog.login/homolog-login.js
+++ homolog.login/homolog-login.js
@@ -1,11 +1,31 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+function dados() {
 
-registerBtn.addEventListener('click', () => {
-  container.classList.add("active");
-});
+  const ds = [
+    {id:0, login:"Matheus", password:"12345", email:"matheuse@gmail.com"}, //0
+    {id:1, login:"Lazarini", password:"67891", email:"lazarini@gmail.com"}, //1
+    {id:2, login:"Wali", password:"147258", email:"wali@gmail.cpom"}, //2
+    {id:3, login:"Luiz", password:"40028922", email:"luiz@gmail.com"} //3
+  ]
+return ds
+}
 
-loginBtn.addEventListener('click', () => {
-  container.classList.remove("active");
-}); 
+function login() {
+
+let login = document.querySelector("#login").value
+let password = document.querySelector("#password").value
+
+const usuarios = dados()
+
+  for(let i = 0; i < usuarios.length; i++) { //laço for
+
+      if (login == usuarios[i].login && password == usuarios[i].password) {
+          console.log("Você logou!")
+          alert('Você logou!')
+      }
+      else if (login != usuarios[i].login && password != usuarios[i].password) {
+        console.log("Usuario ou senha incorretos")
+        alert("Usuario ou senha incorretos")
+      }
+  }
+    
+}
